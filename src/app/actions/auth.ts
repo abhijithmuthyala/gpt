7f7eaa533b0f83116f415de8beb8ae997de46e3d
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2025-01-20 15:25:42 +0530

    Setup basic auth

---
+++ src/app/actions/auth.ts
@@ -0,0 +1,39 @@
+'use server'
+
+import { redirect } from 'next/navigation'
+
+import { createClient } from '@/supabase/server'
+
+export async function login(formData: FormData) {
+  const supabase = await createClient()
+  const credentials = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+  const { error } = await supabase.auth.signInWithPassword(credentials)
+
+  if (error) {
+    console.log(error, "💣💣💣");
+    return error
+  }
+
+  // revalidatePath('/', 'layout')
+  redirect('/')
+}
+
+export async function signup(formData: FormData) {
+  const supabase = await createClient()
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+  const { error } = await supabase.auth.signUp(data)
+
+  if (error) {
+    console.log(error, "💣💣💣");
+    return error
+  }
+
+  // revalidatePath('/', 'layout')
+  redirect('/')
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+import { createClient } from "@/supabase/server";
+import { notFound } from "next/navigation";
+
+export default async function Page() {
+  const { auth } = await createClient();
+  const { data, error } = await auth.getUser();
+
+  if (error) {
+    return notFound();
+  }
+
+  const userName = (data.user.email as string).split("@")[0];
+
+  return (
+    <div>
+      <h1>Hello, {userName}!</h1>
+    </div>
+  );
 }
