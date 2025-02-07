60b5af696ba15bde88d2b9d955bafdb5da435dd0
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2025-02-07 15:51:11 +0530

    Change loading ui

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,19 +3,14 @@ import NewChat from "../components/NewChat";
 export default function HomePage() {
   return (
     <main className="flex flex-col items-center justify-center px-6 relative">
-      <div
-        className="absolute inset-0 animate-grid-fade"
-        style={{
-          backgroundImage: `url("/grid.svg")`,
-        }}
-      />
+      <div className="absolute inset-0 animate-grid-fade bg-grid" />
       <div className="mb-8 max-w-xl text-center text-balance">
         <h1 className="text-4xl font-bold mb-2 text-lime-900">
           Experience Gemini&apos;s Intelligence
         </h1>
         <p className="text-lg text-lime-700">
-          Unlock powerful conversations with Google&apos;s most advanced AI.
-          Code, create, analyze and learn - all in one place.
+          Unlock powerful conversations with Google&apos;s advanced AI. Code,
+          create, analyze and learn - all in one place.
         </p>
       </div>
       <NewChat />
